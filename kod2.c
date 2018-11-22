629dcc81aa16e7cd9ab94d60303348d205a80a8d
zbrylsi <44462185+zbrylsi@users.noreply.github.com>
2018-11-22 10:59:20 +0100

    v1
    wersja 1

---
+++ kod2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+        int liczba1, liczba2;
+
+        printf("Podaj liczbe 1:");
+        scanf("%d",&liczba1);
+        fflush(stdin);
+
+        printf("Podaj liczbe 2:");
+        scanf("%d",&liczba2);
+        fflush(stdin);
+
+        return 0;
+        system("pause");
+}
+
+
